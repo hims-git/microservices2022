d78c9a722db4da35674c1e43da27b6e732c32bcf
Himanshu C <himanshuchaudhary87@gmail.com>
2022-07-22 10:34:40 +0530

    Resilience4J program

---
+++ resilience4j-service/src/main/java/com/him/HttpBinService.java
@@ -0,0 +1,43 @@
+package com.him;
+
+import java.util.Map;
+import java.util.function.Supplier;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HttpBinService {
+
+	public HttpBinService() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Autowired
+	private RestTemplate rest;
+
+	public HttpBinService(RestTemplate rest) {
+		super();
+		this.rest = rest;
+	}
+	
+	
+	public Map get() {
+		return rest.getForObject("https://httpbin.org/get", Map.class);
+	}
+	
+	public Map delay(int seconds) {
+		return rest.getForObject("https://httpbin.org/delay/" +  seconds, Map.class);
+	}
+	
+	/*
+	 * Need this wrapper on top of delay method to make return type 
+	 * of Supplier<Map>, which is required by Resilience4j CurcuitBreaker
+	 */
+	public Supplier<Map> delaySupplier(int seconds) {
+		return () -> this.delay(seconds);
+	}
+	
+
+}
